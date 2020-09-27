bdc777af24d38601f6e66d702a325d436cd809ca
Ronny <mwithiamoki@gmail.com>
2020-09-27 23:36:49 +0300

    class config

---
+++ config.py
@@ -0,0 +1,17 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ronny@localhost/blog'
+    UPLOADED_PHOTOS_DEST ='app/static'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+    # email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
