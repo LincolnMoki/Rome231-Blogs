10c1dc79efa8d131b2877e741e8b14b05281e641
Ronny <mwithiamoki@gmail.com>
2020-09-27 23:55:12 +0300

    class devconfig

--- config.py
+++ config.py
@@ -34,3 +34,22 @@ class TestConfig(Config):
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ronny:rome231@localhost/blog_test'
 
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ronny:rome231@localhost/blog'
+
+    DEBUG = True
+    ENV = 'development'
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
+        
